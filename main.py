b1e59ba8e06d62b528200f01954c7297fbf18298
Zainap16 <zvanblerck1@gmail.com>
2023-07-24 08:59:21 +0200

    user_option

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-# user_option = input("Hi, do you wish to withdrawl or deposit?Press d-deposit or w-withdrawl:")
+
+# user_option -->> stores whether the user wants to wthdrawl and or deposit
+user_option = input("Hi, do you wish to withdrawl or deposit?Press d-deposit or w-withdrawl:")
+
+if user_option == 'w':
+    # withdrawl_user_input -->> the amount  the user wishes to withdrawl
+    withdrawl_user_input= input("how much do you wish to withdrawl?")
+elif user_option == 'd':
+    # deposit_user_input -->> the amount  the user wishes to deposit
+    deposit_user_input= input("how much do you wish to deposit?")
+else:
+    print("Invalid input. Try again.")
+    
+
+
+    
+    
 # ignore above until further evalution
 # sincierly team leader
     
