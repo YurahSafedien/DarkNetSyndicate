1140b15995c47180133586061d4ad75e7f549f09
Zainap16 <zvanblerck1@gmail.com>
2023-07-26 09:16:59 +0200

    Account Creation + Log In Forum
    -Account creation
    -Log In Forum
    -Option Forum
    
    Most is completed

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
 
+# HOME FORUM
+
+# if email is found in TEXTFILE and password matches said email , print(Log In successful)
+# !=, print("Login Failed") -- should have a "Forgot password" and or "sign up" as options
+
+# LOG IN FORUM
+
+# user_email_login = input("Enter your email/username: ")
+# user_password_login = input("Enter your password: ") 
+    
+# ACCOUNT CREATION
+account_creation = input("Do you want to create an account? Press 'y' for yes and 'n' for no").lower()
+
+if account_creation =='y':
+    # no numbers are allowed in first and last name
+    user_first_name = input("Enter your first name: ")
+    user_last_name = input("Enter your last name: ")
+
+    # validation for the "@" symbol
+    user_email_create = input("Enter your email address: ")
+
+    # future updates: may add a specila character, uppercase and lowercase characters as well as numbers
+    user_password_create = input("Enter your password:")
+
+
+    # comfirm_user_password == user_password , != then print("Passwords are not matching.Re-enter your password")
+    comfirm_user_password_create  =input("Comifrm password:")
+    
+    if user_email_create  != comfirm_user_password_create :
+        print("Your passwords are not matching. Please re-enter your passwords.")  
+    else:
+        print("Accoumt created!")
+    
+
+# OPTION FORUM
+
 # user_option -->> stores whether the user wants to wthdrawl and or deposit
 user_option = input("Hi, do you wish to withdrawl or deposit?Press d-deposit or w-withdrawl:")
 
@@ -10,11 +46,7 @@ elif user_option == 'd':
     deposit_user_input= input("how much do you wish to deposit?")
 else:
     print("Invalid input. Try again.")
-    
 
-
-    
-    
 # ignore above until further evalution
 # sincierly team leader
     
